759fc37bbe60fb7cb45a7f64e1d7d575d4ab8d27
byMeier <mateo.leotta@gmail.com>
2024-02-14 18:56:37 -0300

    NavBar v1.0

---
+++ src/main.js
@@ -0,0 +1,27 @@
+// // DECLARACIÓN DE VARIABLES
+
+// // let nav = document.querySelector("#nav");
+// let header = document.querySelector("#header")
+// // let burguer = document.querySelector("#burguer")
+// // let scrolldown = document.querySelector("#scroll-down")
+
+// // HEADER BLUR
+
+// window.onscroll = function () {
+//     let y1 = window.scrollY;
+
+//     // Manejo del primer fragmento de código
+//     if (y1 > 5) {
+//         header.classList.add("blur");
+//     } else {
+//         header.classList.remove("blur");
+//     }
+
+//     // // Manejo del segundo fragmento de código
+//     // let y2 = window.scrollY;
+//     // if (y2 > 10) {
+//     //     scrolldown.classList.add("goodbye");
+//     // } else {
+//     //     scrolldown.classList.remove("goodbye");
+//     // }
+// };
