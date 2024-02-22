41201cd25d510a140c4905cc91a0a388deff0680
byMeier <mateo.leotta@gmail.com>
2024-02-22 00:03:03 -0300

    NavBar Blur + Footer Estático v1.0

--- src/main.js
+++ src/main.js
@@ -1,22 +1,22 @@
 // // DECLARACIÓN DE VARIABLES
 
 let nav = document.querySelector(".nav-list");
-// let header = document.querySelector("#header")
+let header = document.querySelector(".header")
 let burguer = document.querySelector("#burguer")
 // // let scrolldown = document.querySelector("#scroll-down")
 
 // // HEADER BLUR
 
-// window.onscroll = function () {
-//     let y1 = window.scrollY;
+window.onscroll = function () {
+    let y = window.scrollY;
 
-//     // Manejo del primer fragmento de código
-//     if (y1 > 5) {
-//         header.classList.add("blur");
-//     } else {
-//         header.classList.remove("blur");
-//     }
-
+    // Manejo del primer fragmento de código
+    if (y > 5) {
+        header.classList.add("blured");
+    } else {
+        header.classList.remove("blured");
+    }
+};
 //     // // Manejo del segundo fragmento de código
 //     // let y2 = window.scrollY;
 //     // if (y2 > 10) {
